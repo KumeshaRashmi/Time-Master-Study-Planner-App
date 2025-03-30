052749d067d3738878700ae0c0259dae7f1342be
OshaniKR <kawshalyaoshani5@gmail.com>
2025-03-30 09:16:54 +0530

    Signin correct

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
 
-
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -11,7 +10,8 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
